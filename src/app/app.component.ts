85ba4b9007a12c6afd46123ab08b22e8870d7378
Sushmitha Ummanni <sushmitha.u@a3ventures.org>
2020-04-16 22:32:36 +0530

    add all files

---
+++ src/app/app.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'directives';
+  people: any[] = [
+    {
+      "name": "Douglas  Pace",
+      "age": 35,
+      "country": 'MARS'
+    },
+    {
+      "name": "Mcleod  Mueller",
+      "age": 32,
+      "country": 'USA'
+    },
+    {
+      "name": "Day  Meyers",
+      "age": 21,
+      "country": 'HK'
+    },
+    {
+      "name": "Aguirre  Ellis",
+      "age": 34,
+      "country": 'UK'
+    },
+    {
+      "name": "Cook  Tyson",
+      "age": 32,
+      "country": 'USA'
+    }
+  ];
+  
+
+  peopleByCountry: any[] = [
+    {
+      'country': 'UK',
+      'people': [
+        {
+          "name": "Douglas  Pace"
+        },
+        {
+          "name": "Mcleod  Mueller"
+        },
+      ]
+    },
+    {
+      'country': 'US',
+      'people': [
+        {
+          "name": "Day  Meyers"
+        },
+        {
+          "name": "Aguirre  Ellis"
+        },
+        {
+          "name": "Cook  Tyson"
+        }
+      ]
+    }
+  ];  
+}
